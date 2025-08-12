8b060701289fcc9b24d97b8d2e064817f4d7d82f
Maria Cecilia <ceciliamaria1918@gmail.com>
2025-08-12 15:59:32 -0300

    aula03 operadores

---
+++ aula.js
@@ -0,0 +1,15 @@
+let a = 1;
+let b = 5;
+let c = 6;
+
+let delta = ((b*b)-(4*a*c))
+
+let raiz = Math.sqrt(delta)
+
+let x1 = (-b + raiz)/(2*a)
+let x2 = (-b - raiz)/(2*a)
+
+console.log(delta);
+console.log(raiz);
+console.log(x1);
+console.log(x2);
