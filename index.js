c9b4fa87cbca41c04c8f1997bdb5060912f33d08
Mcecilia <ceciliaaraujo1918@gmail.com>
2025-08-28 14:19:32 -0300

    Atualizando atvd

---
+++ index.js
@@ -0,0 +1,50 @@
+for (let i = 0; i <= 5; i++) {
+    console.log(i)
+}
+
+let contador=0;
+
+while (contador<3) {
+    console.log("Contagem: " + contador);
+    contador++;
+}
+
+let timesDeFuteol = ["santa cruz","sport","náutico","smilinguidos"];
+console.log(timesDeFuteol[0])
+console.log(timesDeFuteol[3])
+
+let minhaListaDeTarefas = [];
+minhaListaDeTarefas.push("Comprar pão", "Estudar JavaScript", "Fazer exercícios");
+console.log(minhaListaDeTarefas) 
+
+let animais = ["cookie","aria","lilica","solange","marley"];
+
+for (let i = 0; i<=4; i++) {
+    console.log("O animal na posição " + i + " é " + animais[i])
+} 
+
+let numeros = [7,12,5,20,15,8,3,22,11,4];
+
+for (let i = 0; i<=9; i++) {
+    if ((numeros[i]) % 2 == 0){
+        console.log(numeros[i])
+    }
+}
+
+let i = 0
+let frutas =["maçã","banana","uva","morango","abacaxi"]
+
+while (i<=4) {
+    console.log(frutas[i]);
+    i++;
+}
+console.log("fim da lista")
+
+
+for (let i = 2; i<=9; i++) {
+    console.log("Tabuada do " + i)
+
+    for (let j = 1; j<=10; j++){
+        console.log(`${i}x${j}=` + (i*j))
+    }
+}
